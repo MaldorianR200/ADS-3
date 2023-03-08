339b298a8133475b57b40f038496663c50096844
Stanislav Zelenkov <124909037+MaldorianR200@users.noreply.github.com>
2023-03-08 23:11:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,40 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
+  int count = 0;
+  int left = 0;
+  int right = size - 1;
+  int mid = 0;
+  while (left <= right){
+    mid = (left + right) / 2;
+    if (arr[mid] == value) {
+      int save = mid - 1;
+      while (arr[mid] == value) {
+        if (arr[mid] == value) {
+          ++mid;
+          ++count;
+        } else {
+          break;
+        }
+      }
+      while (arr[save] == value) {
+        if (arr[save] == value) {
+          --save;
+          ++count;
+        } else {
+          break;
+        }
+      }
+      break;
+    }
+    if (arr[mid] > value) {
+      right = mid = 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  if (count > 0) {
+    return count;
+  }
   return 0; // если ничего не найдено
 }
